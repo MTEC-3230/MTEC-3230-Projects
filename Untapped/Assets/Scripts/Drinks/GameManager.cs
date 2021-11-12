5f50636e8c0b12ea85ca13313656fdba8026b2f4
Sky Rolnick <skymeson@gmail.com>
2021-11-12 02:20:24 -0500

    Update from lecture.

--- Untapped/Assets/Scripts/Drinks/GameManager.cs
+++ Untapped/Assets/Scripts/Drinks/GameManager.cs
@@ -11,5 +11,22 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         // Test whether Drink Container contains the same elements as current Recipe
+
+        int num_contained_ingredients = 0;
+        foreach (var drink in currentRecipe.ingredients)
+        {
+            
+            if(currentDrink.contents.Contains(drink))
+            {
+
+                num_contained_ingredients++; 
+            }
+        }
+
+        if(num_contained_ingredients == currentRecipe.ingredients.Count)
+        {
+            // You made a Drink!!!
+
+        }
     }
 }
